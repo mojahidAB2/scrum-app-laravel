27ff3469a634c9c87b71363cf61a3257280608f5
ELAMMARI Oumaima <elammarioumaima46@gmail.com>
2025-04-30 00:00:30 +0200

    ume modification

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+/*Route::get('/creerprojet', function () {
+    return view('creationprojet');
+});
+*/
+
+Route::get('/teams', function () {
+    return view('team');
+});
+
+Route::get('/creerprojet', ['\App\Http\Controllers\ProjectController','index']);
